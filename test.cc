aa4167339cdbca422ffd7a3114a28709fc862635
tom <fanxy@popcap.com>
2018-08-09 18:21:15 +0800

    fix typo error

--- test.cc
+++ test.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 int main()
 {
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
-	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
+	std::cout << "hello world" << std::endl;
 }
