f467408f35ff3e3709b81e1355876cb09deb2041
tom <fanxy@popcap.com>
2018-08-09 18:20:36 +0800

    add multiline for print

--- test.cc
+++ test.cc
@@ -1,5 +1,11 @@
 #include <iostream>
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
+	std::cout << "hello wrold" << std::endl;
 }
